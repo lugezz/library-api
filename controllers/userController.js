86fc14a15698f5286e9b5338c0ee7f2a704f6469
Eugenio <lugezz@gmail.com>
2023-07-02 08:00:49 -0300

    Headers in response

--- controllers/userController.js
+++ controllers/userController.js
@@ -55,7 +55,6 @@ exports.login = async (req, res, next) => {
             req.session.user = user;
 
             res.status(200).json({
-                headers: req.headers,
                 status: 'success',
                 data: {
                     user,
@@ -63,6 +62,15 @@ exports.login = async (req, res, next) => {
                     token: req.sessionID
                 },
                 message: 'Logged in successfully'
+            });
+            res.writeHead(200, {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Credentials': 'true',
+                'Access-Control-Allow-Origin': 'http://localhost:5173',
+                'Access-Control-Allow-Methods': 'POST',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Expose-Headers': 'Set-Cookie',
+                'Set-Cookie': 'sessionID=' + req.sessionID + '; HttpOnly'
             });
             req.session.save();
         } else {
